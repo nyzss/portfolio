11f8bda2f1ba42728225ec083c15019155c2a8c1
Okan Koca <oknakoca@gmail.com>
2024-11-10 18:35:14 +0100

    better overlay on project cards

--- src/components/projects/project.tsx
+++ src/components/projects/project.tsx
@@ -3,18 +3,18 @@ import { Project } from "@/app/projects/projects";
 
 export const SingleProject = ({ project }: { project: Project }) => {
     return (
-        <div className="relative group overflow-hidden rounded-md shadow-lg transform transition-all duration-300 hover:scale-105 h-96">
+        <div className="relative group overflow-hidden rounded-md shadow-lg transform transition-all duration-300 hover:scale-105 h-96 drop-shadow-xl">
             <img
                 src={project.imageUrl}
                 alt={project.name}
-                className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110"
+                className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110 drop-shadow-xl"
             />
 
-            <div className="absolute bottom-0 left-0 w-full bg-black bg-opacity-70 opacity-0 group-hover:opacity-100 transition-opacity duration-300 p-4">
-                <h2 className="text-xl font-bold text-white mb-2">
+            <div className="absolute bottom-0 left-0 w-full  bg-black bg-opacity-15 opacity-0 group-hover:opacity-100 transition-opacity duration-300 p-4 drop-shadow-xl">
+                <h2 className="text-xl font-bold text-primary mb-2">
                     {project.name}
                 </h2>
-                <p className="text-white text-opacity-80 mb-4">
+                <p className="text-primary text-opacity-80 mb-4">
                     {project.description}
                 </p>
                 <div className="flex space-x-4">
