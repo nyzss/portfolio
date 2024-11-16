c35db339eb2307cb12d55e8fdc3f4001fd3ef75a
Okan Koca <oknakoca@gmail.com>
2024-11-16 11:18:11 +0100

    project page is also done

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,18 +1,16 @@
-// import { SingleProject } from "@/components/projects/project";
-// import { projects } from "./projects";
+import { ProjectList } from "@/components/projects/project-list";
+import Link from "next/link";
 
 const Project = () => {
     return (
-        <div className="container max-w-4xl mx-auto py-12 px-4">
-            <h1 className="text-4xl font-medium text-center mb-12">
-                My Projects
-            </h1>
-            <h1 className="text-5xl font-bold text-center">Coming soon...</h1>
-            {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
-                {projects.map((project) => (
-                    <SingleProject key={project.id} project={project} />
-                ))}
-            </div> */}
+        <div className="container max-w-7xl mx-auto py-12 px-4">
+            <Link
+                href="/"
+                className="text-lg font-medium text-teal-500 hover:underline hover:text-teal-600"
+            >
+                Go back
+            </Link>
+            <ProjectList />
         </div>
     );
 };
